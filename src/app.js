642a3a818d3bd1df85be7b3cc83c504062c2f83e
WFH <loving23aa@hotmail.com>
2019-09-24 05:22:48 +0800

    modified file .gitignore

--- src/app.js
+++ src/app.js
@@ -5,15 +5,14 @@ const conf = require('./myConfig/myDefaultConfig');
 
 // 创建服务
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Server Start Success</h1>');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Server Start Success</h1>');
 });
 
 // 服务的监听端口
 server.listen(conf.port, conf.hostname, () => {
-   // 要想${}语法生效，必须使用反引号 ``
-   const addr = `http://${conf.hostname}:${conf.port}`;
-   console.info(`Server start at ${chalk.green(addr)}`);
+  // 要想${}语法生效，必须使用反引号 ``
+  const addr = `http://${conf.hostname}:${conf.port}`;
+  console.info(`Server start at ${chalk.green(addr)}`);
 });
-
